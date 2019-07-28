6dce9dbd2d07896e3ba905d0606f7ee7a02b311e
Taylor Hodge <j.taylor.hodge@gmail.com>
2019-07-28 17:08:34 -0400

    Add options

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,11 @@ clear();
 console.log(
   chalk.blue(figlet.textSync("oyster-cli", { horizontalLayout: "full" }))
 );
+
+program
+  .version("0.0.1")
+  .description("An example CLI for ordering oysters")
+  .option("-a, --amethyst", "Amethyst Oysters")
+  .option("-b, --blue-point", "Blue Point Oysters")
+  .option("-c, --chelsea-gem", "Chelsea Gem Oysters")
+  .parse(process.argv);
